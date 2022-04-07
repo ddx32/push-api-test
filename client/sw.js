18a1ff7462a31f9d902de5f400ef7713db4ca638
Filip Ruzicka <filip.ruzicka@prusa3d.cz>
2022-04-07 14:36:56 +0200

    Enrichen the notification

--- client/sw.js
+++ client/sw.js
@@ -1,4 +1,18 @@
-self.addEventListener("push", () => {
+self.addEventListener("push", (event) => {
   console.log(self.registration);
-  self.registration.showNotification("This is not a drill", {});
+  let data = {}
+  if (event.data) {
+    data = event.data.json()
+  }
+
+  var notification = new self.Notification(data.title || 'Default title', {
+    body: data.message || 'Default message',
+    tag: 'sample notification'
+  })
+  
+  notification.addEventListener('click', function() {
+    if (clients.openWindow) {
+      clients.openWindow('https://connect.prusa3d.com');
+    }
+  })
 });
