cd51245acfd4cd717d9417359eba9f91afbaaab2
Filip Ruzicka <filip.ruzicka@prusa3d.cz>
2022-04-07 14:42:32 +0200

    fix

--- client/sw.js
+++ client/sw.js
@@ -2,19 +2,18 @@ self.addEventListener("push", (event) => {
   console.log(self.registration);
   let data = {}
   if (event.data) {
-    data = event.data
+    data = event.data.json()
   }
+  self.registration.showNotification("Sample notification", {});
 
-  const title = data.title || 'Default title';
-
-  var notification = new self.Notification(title, {
-    body: data.message || 'Default message',
-    tag: 'sample notification'
-  })
+  // var notification = new self.Notification(title, {
+  //   body: data.message || 'Default message',
+  //   tag: 'sample notification'
+  // })
   
-  notification.addEventListener('click', function() {
-    if (clients.openWindow) {
-      clients.openWindow('https://connect.prusa3d.com');
-    }
-  })
+  // notification.addEventListener('click', function() {
+  //   if (clients.openWindow) {
+  //     clients.openWindow('https://connect.prusa3d.com');
+  //   }
+  // })
 });
